091529df1f7db4e387df28fa9d2f45ed486ca349
Kei-Saka <kei.sakanishi@gmail.com>
2022-08-09 05:13:44 +0900

    ♻️ カードをシンプルに

--- components/organisms/PortfolioSection.tsx
+++ components/organisms/PortfolioSection.tsx
@@ -1,21 +1,22 @@
 import { PrimaryButton } from "../atoms/PrimaryButton";
 import { Grid } from "@mantine/core";
+import { ArticleCard } from "../ArticleCard";
 
 export const PortfolioSection = () => {
   return (
     <div>
       <Grid>
         <Grid.Col md={6} lg={3}>
-          1
+          <ArticleCard />
         </Grid.Col>
         <Grid.Col md={6} lg={3}>
-          2
+          <ArticleCard />
         </Grid.Col>
         <Grid.Col md={6} lg={3}>
-          3
+          <ArticleCard />
         </Grid.Col>
         <Grid.Col md={6} lg={3}>
-          4
+          <ArticleCard />
         </Grid.Col>
       </Grid>
       <PrimaryButton value="View All" />
