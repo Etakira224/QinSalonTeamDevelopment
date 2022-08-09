a3002fb80ae6eb086f6294dc14e6eef1ec570397
Kei-Saka <kei.sakanishi@gmail.com>
2022-08-09 23:52:40 +0900

    💄 コンテンツをテンプレート化しようかな

--- components/TitleText.tsx
+++ components/TitleText.tsx
@@ -1,16 +1,17 @@
 //unuse. 記述量少ないし直接使うのがよさそう
+//作戦変更 コンテンツのテンプレートで使うことにした
 
 import { Title } from '@mantine/core';
 
 export const TitleText = () => {
   return (
     <>
-      <Title order={1}>This is h1 title</Title>
-      <Title order={2}>This is h2 title</Title>
+      {/* <Title order={1}>This is h1 title</Title>
+      <Title order={2}>This is h2 title</Title> */}
       <Title order={3}>This is h3 title</Title>
-      <Title order={4}>This is h4 title</Title>
+      {/* <Title order={4}>This is h4 title</Title>
       <Title order={5}>This is h5 title</Title>
-      <Title order={6}>This is h6 title</Title>
+      <Title order={6}>This is h6 title</Title> */}
     </>
   );
 }
--- components/templates/SectionTemplate.tsx
+++ components/templates/SectionTemplate.tsx
@@ -1,5 +1,13 @@
-export const SectionTemplate = () => {
+import { ReactElement } from "react";
+import { PrimaryButton } from "../atoms/PrimaryButton";
+import { TitleText } from "../TitleText";
+
+export const SectionTemplate = (props: { children: ReactElement }) => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <TitleText />
+      {props.children}
+      <PrimaryButton value={""} />
+    </div>
+  );
+};
